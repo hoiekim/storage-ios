291ffb068c8940e683ba9560787bc317d06a5d7d
hoiekim <github@hoie.kim>
2024-11-25 07:38:47 -0800

    Initial Commit

---
+++ Hoicloud/HoicloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoicloudApp.swift
+//  Hoicloud
+//
+//  Created by Hoie Kim on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct HoicloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoicloudTests/HoicloudTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoicloudTests.swift
+//  HoicloudTests
+//
+//  Created by Hoie Kim on 11/25/24.
+//
+
+import Testing
+@testable import Hoicloud
+
+struct HoicloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
